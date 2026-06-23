67d1e80625b4a3da7c12b227c2e6de6f1a52cbe2
pixelmash <239129817+pixelmash@users.noreply.github.com>
2026-06-23 16:04:44 +0200

    initial project scaffold (Karate + Playwright setup)

---
+++ karate/src/test/java/shadymeadows/KarateRunner.java
@@ -0,0 +1,11 @@
+package shadymeadows;
+
+import com.intuit.karate.junit5.Karate;
+
+class KarateRunner {
+
+    @Karate.Test
+    Karate testAll() {
+        return Karate.run().relativeTo(getClass());
+    }
+}
